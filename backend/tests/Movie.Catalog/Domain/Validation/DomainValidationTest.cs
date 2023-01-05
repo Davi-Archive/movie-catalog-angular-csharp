72cf70de3044a7f2eec58ca4b86e4574c4857dd4
d1av <davi4alves@gmail.com>
2023-01-05 12:45:45 -0300

    test: length validation

--- backend/tests/Movie.Catalog/Domain/Validation/DomainValidationTest.cs
+++ backend/tests/Movie.Catalog/Domain/Validation/DomainValidationTest.cs
@@ -1,4 +1,5 @@
 ﻿using Bogus;
+using FluentAssertions;
 using Movie.Catalog.Domain.Exceptions;
 using Movie.Catalog.Domain.Validation;
 using Xunit;
@@ -35,15 +36,60 @@ namespace Movie.Catalog.UnitTests.Domain.Validation
         [Trait("Domain", "DomainValidation - Validation")]
         public void NotNull()
         {
-            string value = null;
+            string? value = null;
             Action action = () => DomainValidation.NotNull(value, "Value");
 
             Assert.Throws<EntityValidationException>(action);
         }
 
 
+
+        [Theory(DisplayName = nameof(NotNullOrEmptyThrowWhenEmpty))]
+        [Trait("Domain", "DomainValidation - Validation")]
+        [InlineData("")]
+        [InlineData("   ")]
+        [InlineData(null)]
+        public void NotNullOrEmptyThrowWhenEmpty(string? target)
+        {
+            Action action =
+                () => DomainValidation.NotNullOrEmpty(target, "fieldName");
+
+
+            var exception = Assert.Throws<EntityValidationException>(action);
+            Assert.Equal("fieldName should not be null or empty", exception.Message);
+        }
+
+        [Fact(DisplayName = nameof(NotNullOrEmptyOk))]
+        [Trait("Domain", "DomainValidation - Validation")]
+        public void NotNullOrEmptyOk()
+        {
+            var target = Faker.Commerce.ProductName();
+
+
+            Action action =
+                () => DomainValidation.NotNullOrEmpty(target, "fieldName");
+
+
+            action.Should().NotThrow();
+        }
+
+        // min length
+
+        [Theory(DisplayName = nameof(MinLengthThrowWhenLess))]
+        [Trait("Domain", "DomainValidation - Validation")]
+        public void MinLengthThrowWhenLess(string target, int minLength)
+        {
+            Action action =
+                () => DomainValidation.MinLength(target, minLength, "fieldName");
+
+            action.Should().Throw<EntityValidationException>()
+                .WithMessage($"fieldName should not be less than {minLength} long");
+        }
+
+        public static IEnumerable<object[]> GetValuesSmallerThan { get; }
+
+        // max length
+
     }
-    // min length
-    // max length
 }
 
