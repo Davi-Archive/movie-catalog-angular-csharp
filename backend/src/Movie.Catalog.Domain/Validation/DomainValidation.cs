a89520d4b9425904512c388b0214e73bdb8023ef
d1av <davi4alves@gmail.com>
2023-01-05 12:20:21 -0300

    test: validation tests

---
+++ backend/src/Movie.Catalog.Domain/Validation/DomainValidation.cs
@@ -0,0 +1,13 @@
+﻿using Movie.Catalog.Domain.Exceptions;
+
+namespace Movie.Catalog.Domain.Validation
+{
+    public class DomainValidation
+    {
+        public static void NotNull(object target, string fieldName)
+        {
+            if (target is null)
+                throw new EntityValidationException($"{fieldName} should not be null");
+        }
+    }
+}
---
+++ backend/tests/Movie.Catalog/Domain/Validation/DomainValidationTest.cs
@@ -0,0 +1,49 @@
+﻿using Bogus;
+using Movie.Catalog.Domain.Exceptions;
+using Movie.Catalog.Domain.Validation;
+using Xunit;
+
+namespace Movie.Catalog.UnitTests.Domain.Validation
+{
+    public class DomainValidationTest
+    {
+
+        public Faker Faker { get; set; } = new Faker();
+
+        // not null
+        [Fact(DisplayName = nameof(NotNullOk))]
+        [Trait("Domain", "DomainValidation - Validation")]
+        public void NotNullOk()
+        {
+            var value = Faker.Commerce.ProductName();
+            try
+            {
+                Action action = () => DomainValidation.NotNull(value, "Value");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail($"Domain Validation did throw: {ex.Message}");
+            }
+
+
+        }
+
+
+        // not null or empty
+
+        [Fact(DisplayName = nameof(NotNull))]
+        [Trait("Domain", "DomainValidation - Validation")]
+        public void NotNull()
+        {
+            string value = null;
+            Action action = () => DomainValidation.NotNull(value, "Value");
+
+            Assert.Throws<EntityValidationException>(action);
+        }
+
+
+    }
+    // min length
+    // max length
+}
+
